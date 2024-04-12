d5edfff4c12f25c917002c7973bf85b0fd4eac22
suzzang2 <subin030102@naver.com>
2024-04-12 20:25:09 +0900

    로봇 개수 num 인자로 받기

--- projects/automated_warehouse/automated_warehouse.c
+++ projects/automated_warehouse/automated_warehouse.c
@@ -35,23 +35,39 @@ void test_thread(void* aux){
 void run_automated_warehouse(char **argv)
 {
         init_automated_warehouse(argv); // do not remove this
+        int num = *argv[1] - '0'; // number of robots by argument
 
         printf("implement automated warehouse!\n");
 
         // test case robots
-        robots = malloc(sizeof(struct robot) * 1);
-        setRobot(&robots[0], "R1", 5, 5, 0, 0); //초기에는 무조건 W에 위치해야 함
-        // setRobot(&robots[1], "R2", 0, 2, 0, 0);
-        // setRobot(&robots[2], "R3", 1, 1, 1, 1);
-        // setRobot(&robots[3], "R4", 5, 5, 0, 0);
+        robots = malloc(sizeof(struct robot) * num);
+        // setRobot(&robots[0], "R1", 5, 5, 0, 0); //초기에는 무조건 W에 위치해야 함
+        // // setRobot(&robots[1], "R2", 0, 2, 0, 0);
+        // // setRobot(&robots[2], "R3", 1, 1, 1, 1);
+        // // setRobot(&robots[3], "R4", 5, 5, 0, 0);
+        for (int i = 0; i < num; i++) {
+                char name[4]; // 로봇 이름을 저장할 배열
+                sprintf(name, "R%d", i + 1); // 로봇 이름 생성
+                setRobot(&robots[i], name, 5, 5, 0, 0); // 로봇 설정->모두 W에 배치
+        }
 
+        // // example of create thread
+        // tid_t* threads = malloc(sizeof(tid_t) * (num+1));
+        // int idxs[2] = {0};
+        // threads[0] = thread_create("CNT", 0, &test_cnt, NULL); //중앙제어노드
+        // threads[1] = thread_create("R1", 0, &test_thread, &idxs[0]);
+        // // threads[2] = thread_create("R2", 0, &test_thread, &idxs[2]);
+        // // threads[3] = thread_create("R3", 0, &test_thread, &idxs[3]);
         // example of create thread
-        tid_t* threads = malloc(sizeof(tid_t) * 2);
-        int idxs[2] = {0};
-        threads[0] = thread_create("CNT", 0, &test_cnt, NULL); //중앙제어노드
-        threads[1] = thread_create("R1", 0, &test_thread, &idxs[0]);
-        // threads[2] = thread_create("R2", 0, &test_thread, &idxs[2]);
-        // threads[3] = thread_create("R3", 0, &test_thread, &idxs[3]);
+        tid_t* threads = malloc(sizeof(tid_t) * (num + 1));
+        int idxs[num + 1]; // 인덱스 배열 생성
+        threads[0] = thread_create("CNT", 0, &test_cnt, NULL); // 중앙 제어 노드 스레드
+        for (int i = 0; i < num; i++) {
+            char name[4]; // 로봇 이름을 저장할 배열
+            sprintf(name, "R%d", i + 1); // 로봇 이름 생성
+            idxs[i] = i; // 인덱스 배열 초기화
+            threads[i + 1] = thread_create(name, 0, &test_thread, &idxs[i]); // 로봇 스레드
+        }
 
         // if you want, you can use main thread as a central control node
         
